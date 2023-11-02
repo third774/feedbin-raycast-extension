2810e257ba61525fd2ab5a65ef0ed0c4ecce1af3
Kevin Kipp <kkipp@cloudflare.com>
2023-11-02 11:00:06 -0500

    Fix sync cache utility

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -10,6 +10,10 @@ export function getCachedData<T>(cache: Cache, key: string) {
 
 export function useSyncCache<T>(cache: Cache, key: string, data: T) {
   useEffect(() => {
-    cache.set(key, JSON.stringify(data));
+    if (data) {
+      cache.set(key, JSON.stringify(data));
+    } else {
+      cache.remove(key);
+    }
   }, [data]);
 }
