9e405fe7f3af91d8414f6c94353986cd87a30d23
Kevin Kipp <kevin.kipp@gmail.com>
2023-11-16 22:03:56 -0600

    Wrap refreshMenuBar in try/catch

--- src/utils/refreshMenuBar.ts
+++ src/utils/refreshMenuBar.ts
@@ -1,8 +1,14 @@
 import { launchCommand, LaunchType } from "@raycast/api";
 
-export function refreshMenuBar() {
-  return launchCommand({
-    name: "unread-menu-bar",
-    type: LaunchType.Background,
-  });
+export async function refreshMenuBar() {
+  // wrapping this in a try/catch because users who don't have the
+  // menu command active will get an error, so we can just ignore it
+  try {
+    await launchCommand({
+      name: "unread-menu-bar",
+      type: LaunchType.Background,
+    });
+  } catch {
+    return;
+  }
 }
